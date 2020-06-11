6cbb146255652f2905ad10615bc78e2eab506a5f
Francisco Greco <a320610@uach.mx>
2020-06-10 22:32:51 -0600

    Creation script

---
+++ VisualShare/VisualShare/Server/Migrations/20200611043225_InitialCreate.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisualShare.Server.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<byte[]>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    AuthorName = table.Column<string>(nullable: true),
+                    PublishedDate = table.Column<DateTime>(nullable: false, defaultValueSql: "DATETIME('now')"),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Authors_AuthorName",
+                        column: x => x.AuthorName,
+                        principalTable: "Authors",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Photos_Categories_CategoryName",
+                        column: x => x.CategoryName,
+                        principalTable: "Categories",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PublishedDate = table.Column<DateTimeOffset>(nullable: false, defaultValueSql: "DATETIME('now')"),
+                    Content = table.Column<byte[]>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    AuthorName = table.Column<string>(nullable: true),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Videos_Authors_AuthorName",
+                        column: x => x.AuthorName,
+                        principalTable: "Authors",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Videos_Categories_CategoryName",
+                        column: x => x.CategoryName,
+                        principalTable: "Categories",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(nullable: true),
+                    AuthorName = table.Column<string>(nullable: true),
+                    PublishedDate = table.Column<DateTimeOffset>(nullable: false, defaultValueSql: "DATETIME('now')"),
+                    PhotoId = table.Column<int>(nullable: true),
+                    VideoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Authors_AuthorName",
+                        column: x => x.AuthorName,
+                        principalTable: "Authors",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_Photos_PhotoId",
+                        column: x => x.PhotoId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_Videos_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(nullable: true),
+                    PhotoId = table.Column<int>(nullable: true),
+                    VideoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Likes_Photos_PhotoId",
+                        column: x => x.PhotoId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Likes_Videos_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorName",
+                table: "Comments",
+                column: "AuthorName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PhotoId",
+                table: "Comments",
+                column: "PhotoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_VideoId",
+                table: "Comments",
+                column: "VideoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_PhotoId",
+                table: "Likes",
+                column: "PhotoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_VideoId",
+                table: "Likes",
+                column: "VideoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AuthorName",
+                table: "Photos",
+                column: "AuthorName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_CategoryName",
+                table: "Photos",
+                column: "CategoryName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_AuthorName",
+                table: "Videos",
+                column: "AuthorName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_CategoryName",
+                table: "Videos",
+                column: "CategoryName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Likes");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "Videos");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
